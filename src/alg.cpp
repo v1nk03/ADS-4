9cde68727959ba5eccc70451d53e69d3826a62c6
v1nk03 <vovakonoval823@gmail.com>
2025-04-11 23:52:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,59 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  return 0;
+int countPairs1(int *arr, int arrLength, int targetSum) {
+  int pairCount = 0;
+  for (int i = 0; i < arrLength; i++) {
+    for (int j = i + 1; j < arrLength; j++) {
+      if (arr[i] + arr[j] == targetSum) {
+        pairCount++;
+      }
+    }
+  }
+  return pairCount;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
+
+int countPairs2(int *arr, int arrLength, int targetSum) {
+  int pairCount = 0;
+  int endIndex = arrLength - 1;
+  while (endIndex > 0 && arr[endIndex] > targetSum) {
+    endIndex--;
+  }
+  for (int leftIndex = 0; leftIndex < arrLength; leftIndex++) {
+    for (int rightIndex = endIndex; rightIndex > leftIndex; rightIndex--) {
+      if (arr[leftIndex] + arr[rightIndex] == targetSum) {
+        pairCount++;
+      }
+    }
+  }
+  return pairCount;
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
+
+int countPairs3(int *arr, int arrLength, int targetSum) {
+  int pairCount = 0;
+  for (int i = 0; i < arrLength; i++) {
+    int complement = targetSum - arr[i];
+    int leftIndex = i + 1;
+    int rightIndex = arrLength - 1;
+    while (leftIndex <= rightIndex) {
+      int middleIndex = leftIndex + (rightIndex - leftIndex) / 2;
+      if (arr[middleIndex] == complement) {
+        pairCount++;
+        int j = middleIndex - 1;
+        while (j >= leftIndex && arr[j] == complement) {
+          pairCount++;
+          j--;
+        }
+        j = middleIndex + 1;
+        while (j <= rightIndex && arr[j] == complement) {
+          pairCount++;
+          j++;
+        }
+        break;
+      } else if (arr[middleIndex] < complement) {
+        leftIndex = middleIndex + 1;
+      } else {
+        rightIndex = middleIndex - 1;
+      }
+    }
+  }
+  return pairCount;
 }
